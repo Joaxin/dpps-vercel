ce33cefc401c08c337295132d6e9780c830ed65b
joaxin <lotility@gmail.com>
2021-06-05 16:54:15 +0800

    responsive rebuild:art:

--- musica/views.py
+++ musica/views.py
@@ -3,6 +3,7 @@ from .scripts.neteaseMusicAPI import song_infos
 from django.http import HttpResponse
 from datetime import datetime
 # Create your views here.
+from urllib.parse import quote
 import re
 
 
@@ -11,18 +12,22 @@ def home(request):
 
 
 def netease_infos(request):
-    sid = request.GET['sid']
+    sid = quote(request.GET['sid'])
     now = datetime.now()
-    pattern = re.compile('song\?id=(\d+).*', re.S)
+    pattern = re.compile('song%3Fid%3D(\d+).*', re.S)
     if re.search("^(\d+)$", sid):
         sid = re.search("^(\d+)$", sid).group(1)
     elif re.search(pattern, sid):
         sid = re.search(pattern, sid).group(1)
+    ## https://music.163.com/#/song?id=192316 有#的会无法正常解析，会解析成https://music.163.com/
+    ## 当url中出现"#"号时，"#"及其后面的字符串都会被忽略，不会被发送到服务器，因为浏览器将一个url视为一个html页面
+    ## The "#" character marks inline anchors (links within the same page) in a URL, so the browser will never send it to Django.
+    ## SEE https://en.wikipedia.org/wiki/Percent-encoding
     else:
         html = f'''
 	    <html>
 	        <body>
-	            <h1>非法输入！/h1>
+	            <h1>非法的URL或ID！</h1>
 	        </body>
 	    </html>
 	    '''
