f476cb71a03682f6bd6082aa63a19373107905fe
joaxin <lotility@gmail.com>
2024-11-29 16:04:49 +0800

    update py312

--- dpps/wsgi.py
+++ dpps/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dpps.settings')
 
 application = get_wsgi_application()
+
+pp = get_wsgi_application()
